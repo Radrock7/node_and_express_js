fcb017789e09a88ea932e62ac0a4e82fd3890ed7
Radrock7 <ryansheahedu@gmail.com>
2025-08-20 23:33:39 -0400

    practice

--- promise_callbacks/practice.js
+++ promise_callbacks/practice.js
@@ -1,1 +1,28 @@
-let firstPromise = new Promise
+let firstPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("First promise resolved")
+    },6000)
+});
+
+let secPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Sec promise resolved")
+    },3000)
+});
+
+// run sequentially
+firstPromise.then((successMessage) => {
+    console.log(successMessage);
+});
+
+secPromise.then((successMessage) => {
+    console.log(successMessage);
+});
+
+// output first then second
+firstPromise.then((successMessage) => {
+    console.log("From callback" + successMessage)
+    secPromise.then((successMessage) => {
+        console.log("From callback" + successMessage)
+    })
+})
